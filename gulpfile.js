34d899c5ab9cb57a4ed8e88592e80ae833de9ff0
bdp-adrian <48244858+bdp-adrian@users.noreply.github.com>
2021-08-23 14:07:53 +0000

    Deploying to gh-pages from @ bdp-adrian/klimafit-proto@6b0b16da6e34fda61a240e20084b22457071c121 🚀

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp         = require('gulp'),
+    gulpSass     = require('gulp-sass')(require('sass')),
+    watch        = require('gulp-watch'),
+    uglifycss    = require('gulp-uglifycss'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minify       = require('gulp-minify'),
+    concat       = require('gulp-concat'),
+    bro          = require('gulp-bro');
+    browserSync  = require('browser-sync').create();
+	// TO DO: add sourcemaps
+
+function sass(done) {
+	gulp.src('./sass/**/*.scss')
+		.pipe(gulpSass().on('error', gulpSass.logError))
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(uglifycss({
+			"uglyComments": true
+		}))
+		.pipe(gulp.dest('./dist/'));
+	done();
+};
+
+function js (done) {
+	gulp.src('js/*.js')
+		.pipe(bro())
+		.pipe(minify({
+			ext:{
+				min:'.min.js',
+			},
+			noSource: true,
+		}))
+		.pipe(gulp.dest('./dist'));
+	done();
+}
+
+function browser(done) {
+	browserSync.init({
+		/**
+		 * Set custom proxy server url
+		 */
+		server: {
+            baseDir: "./"
+        }
+	});
+
+	gulp.watch('sass/**/*.scss', sass).on('change', browserSync.reload);
+	gulp.watch('js/**/*.js', js).on('change', browserSync.reload);
+	gulp.watch('*.html', js).on('change', browserSync.reload);
+}
+
+function watchFiles() {
+    gulp.watch('sass/**/*.scss', sass);
+    gulp.watch('js/**/*.js', js);
+}
+
+gulp.task("sass", sass);
+
+gulp.task("js", js);
+
+gulp.task("browser", browser);
+
+gulp.task("default", gulp.parallel(sass,js,watchFiles,browser));
+
+gulp.task('run', gulp.parallel(sass,js));
+
+gulp.task('watch', gulp.parallel(watchFiles));
